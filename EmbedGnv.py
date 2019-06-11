9daa875c6c33ae1e0483379562b70b340d459d11
surajraj99 <42072256+surajraj99@users.noreply.github.com>
2019-06-11 12:25:43 -0400

    Update EmbedGnv.py

--- EmbedGnv.py
+++ EmbedGnv.py
@@ -4,6 +4,7 @@ import gensim
 import numpy as np
 import pickle
 from timeit import default_timer as timer
+from nltk import word_tokenize
 
 f = open('cleaned_notes.pckl', 'rb')
 notes = pickle.load(f)
@@ -24,6 +25,46 @@ for i in range(len(model.vocab)):
     embedding_vector = model[model.index2word[i]]
     if embedding_vector is not None:
         embedding_matrix[i] = embedding_vector
+
+# may potentially need to turn x train and x test data into these vectors based on word vectors
+
+# function to get each word in each do transformed into a 300 dimensional vector; could compress this with average function later to make it faster
+def get_word_vecs(documents):
+	notes_vec = []
+	for doc in documents:
+		doc = []
+		for word in doc:
+			if word in model.vocab:
+				doc.append(model[word])
+		notes_vec.append(doc)
+		
+	return notes_vec 
+
+
+temp_notes = []
+for note in notes:
+    temp_notes.append(word_tokenize(note))
+notes = temp_notes
+
+# function to get the vector for each document from the words themselves; averaging all the vectors in the document
+def document_vector(word2vec_model, doc): # remove out-of-vocabulary words
+    t = []
+    for word in doc:
+        if word in word2vec_model.vocab:
+            t.append(word)
+    if len(t) > 0:
+        return np.mean(word2vec_model[t], axis=0)
+    else:
+        return []
+
+
+doc_vecs = []
+for doc in notes: #look up each doc in model
+	a = document_vector(model, doc)
+	if (len(a) > 0):
+		doc_vecs.append(a)
+
+print("Total number of documents: " + str(len(doc_vecs)))
 
 end = timer() # around 3-4 minutes to run the whole thing
 print("Done within " + str(end-start) + " seconds")
