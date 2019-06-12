e9a9bc2935313e6db64c8dad0bc27d0fb93b1b32
surajraj99 <42072256+surajraj99@users.noreply.github.com>
2019-06-12 10:57:59 -0400

    Update CleanNotes.py

--- CleanNotes.py
+++ CleanNotes.py
@@ -9,73 +9,111 @@ import os
 import csv
 import pickle
 from timeit import default_timer as timer
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
 import inflect
+from autocorrect import spell
+from collections import OrderedDict
 
 # function that cleans text
 # still need to account for contractions, abbreviations, and numbers/fractions
 default_stemmer = PorterStemmer()
 default_stopwords = stopwords.words('english') # or any other list of your choice
-def clean_text(text, ):
+def clean_text(text, replace_numbers = False, remove_rare = False, remove_punctuation = False, stem_text = False, remove_stopwords = False, remove_num = False , spell_check = False, remove_repeat = False):
+        def misc_cleaning(text):
+                text = re.sub("-([a-zA-Z]+)", r"\1", text) # replaces hyphen with spaces in case of strings
+                text = re.sub(' y ', '', text) # gets rid of random y accent stuff scattered through the text
+                text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+                text = re.sub(r"what's", "what is ", text)
+                text = re.sub(r"\'s", " ", text)
+                text = re.sub(r"\'ve", " have ", text)
+                text = re.sub(r"can't", "cannot ", text)
+                text = re.sub(r"n't", " not ", text)
+                text = re.sub(r"i'm", "i am ", text)
+                text = re.sub(r"\'re", " are ", text)
+                text = re.sub(r"\'d", " would ", text)
+                text = re.sub(r"\'ll", " will ", text)
+                text = re.sub(r",", " ", text)
+                text = re.sub(r"\.", " ", text)
+                text = re.sub(r"!", " ! ", text)
+                text = re.sub(r"\/", " ", text)
+                text = re.sub(r"\^", " ^ ", text)
+                text = re.sub(r"\+", " + ", text)
+                text = re.sub(r"\-", " - ", text)
+                text = re.sub(r"\=", " = ", text)
+                text = re.sub(r"'", " ", text)
+                text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+                text = re.sub(r":", " : ", text)
+                text = re.sub(r" e g ", " eg ", text)
+                text = re.sub(r" b g ", " bg ", text)
+                text = re.sub(r" u s ", " american ", text)
+                text = re.sub(r"\0s", "0", text)
+                text = re.sub(r" 9 11 ", "911", text)
+                text = re.sub(r"e - mail", "email", text)
+                text = re.sub(r"j k", "jk", text)
+                text = re.sub(r"\s{2,}", " ", text)
+                return text
 
-        # def replace_numbers(text):
-        #         words = word_tokenize(text)
-        #         p = inflect.engine()
-        #         new_words = []
-        #         for word in words:
-        #                 if word.isdigit():
-        #                         new_word = p.number_to_words(word)
-        #                         new_words.append(new_word)
-        #                 else:
-        #                         new_words.append(word)
-        #         newWords = " ".join(new_words)
-        #         return newWords
+        # function to tokenize text which is used in a lot of the later processing
+        def tokenize_text(text):
+                return [w for s in sent_tokenize(text) for w in word_tokenize(s)]
 
-        def remove_rare(text):
+        text = text.strip(' ') # strip whitespaces
+        text = misc_cleaning(text) # look at function, random cleaning stuff
+        text = text.lower() # lowercase
+
+        if remove_repeat:
+                sentences = sent_tokenize(text)
+                new_sent = list(OrderedDict.fromkeys(sentences))
+                text = " ".join(new_sent)
+        
+        # removes punctuation
+        if remove_punctuation:
+                text = text.translate(str.maketrans('', '', string.punctuation))
+
+        # does a spellcheck of the text and corrects words that are misspelled
+        if spell_check:
+                tokens = []
+                for word in tokenize_text(text):
+                        tokens.append(spell(word))
+                text = " ".join(tokens)
+
+        # optional: replaces numbers ("3") with their word counterparts ("three")
+        if replace_numbers:
+                words = word_tokenize(text)
+                p = inflect.engine()
+                new_words = []
+                for word in words:
+                        if word.isdigit():
+                                new_word = p.number_to_words(word)
+                                new_words.append(new_word)
+                        else:
+                                new_words.append(word)
+                text = " ".join(new_words)
+
+        # optional: removes the rarest words in each text --> right now it's 10
+        if remove_rare(text):
                 tokens = word_tokenize(text)
                 freq_dist = nltk.FreqDist(tokens)
                 rarewords = list(freq_dist.keys())[-10:]
                 new_words = [word for word in tokens if word not in rarewords]
                 text = " ".join(new_words)
-                return text
-                
 
-        def tokenize_text(text):
-                return [w for s in sent_tokenize(text) for w in word_tokenize(s)]
+        # optional: stems text using Porter Stemmer
+        if stem_text:
+                stemmer = default_stemmer
+                tokens = tokenize_text(text)
+                text = " ".join([stemmer.stem(t) for t in tokens])
 
-        def remove_punctuation_characters(text):
-                text = text.translate(str.maketrans('', '', string.punctuation))
-                return text
-
-        # def stem_text(text, stemmer=default_stemmer):
-        #         tokens = tokenize_text(text)
-        #         return ' '.join([stemmer.stem(t) for t in tokens])
-
-        def remove_stopwords(text, stop_words=default_stopwords):
+        # removes stop words such as "a", "the", etc.
+        if remove_stopwords:
+                stop_words = default_stopwords
                 tokens = [w for w in tokenize_text(text) if w not in stop_words]
-                return ' '.join(tokens)
+                text = " ".join(tokens)
         
-        def misc_cleaning(text):
-                text = re.sub("-([a-zA-Z]+)", r"\1", text) # replaces hyphen with spaces in case of strings
-                text = re.sub(r'[0-9\.]+', '', text) # gets rid of numbers --> may not want this in certain cases
-                text = re.sub(' y ', '', text) # gets rid of random y accent stuff scattered through the text
-                text = re.sub(' ml ', ' milliliter ', text)
-                text = re.sub(' mg ', ' milligram ', text)
-                text = re.sub(' kg ', ' kilogram ', text)
-                text = re.sub(' yo ', ' year old ', text)
-                text = re.sub(' cm ', ' centimeter ', text)
-                text = re.sub(' mm ', ' millimeter ', text)
-                return text
-
-        text = text.strip(' ') # strip whitespaces
-        # text = replace_numbers(text)
-        text = misc_cleaning(text) # look at function, random cleaning stuff
-        text = text.lower() # lowercase
-        # text = stem_text(text) # stemming only works for word vectors that are not pre-trained so don't use in this case
-        text = remove_punctuation_characters(text) # remove punctuation and symbols
-        text = remove_stopwords(text) # remove stopwords
-        text = remove_rare(text) # remove 10 most rare words from each document
+        # optional: removes numbers completely from the ext
+        if remove_num:
+                text=text.split()
+                text=[x for x in text if not x.isnumeric()]
+                text= " ".join(text)
 
         return text
 
@@ -84,17 +122,19 @@ f = open('original_notes.pckl', 'rb')
 old_notes = pickle.load(f)
 f.close()
 
+print("Started cleaning process")
 start = timer()
 notes = []
 for note in old_notes: # takes 100 - 300 seconds to go through the cleaning for-loop for all notes
-        notes.append(clean_text(note))
+        notes.append(clean_text(note, remove_punctuation = True, remove_stopwords = True, spell_check = True, remove_repeat = True))
 end = timer()
 print(end - start)
-
+print("Ended cleaning progress")
 
 # save cleaned notes into a picle file
 f = open('cleaned_notes.pckl', 'wb')
 pickle.dump(notes, f)
 f.close()
+print("Saved cleansed notes")
 
 print("Done cleaning and saving within " + str(end-start) + " seconds")
