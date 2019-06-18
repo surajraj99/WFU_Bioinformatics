e2446f6ce3871ddfd0faf005d0723d193a2f3c97
Suraj Rajendran <42072256+surajraj99@users.noreply.github.com>
2019-06-18 14:14:07 -0400

    Update DLModels.py

--- DLModels.py
+++ DLModels.py
@@ -1,15 +1,27 @@
 import os
+import random
+import statistics as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+import pandas as pd
+
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Input, LSTM, Embedding, Dropout, GRU, Bidirectional
 from keras.layers import Flatten, Conv1D, MaxPooling1D, GlobalMaxPooling1D
 from keras.models import Model
 from keras import metrics
-import pickle
-import numpy as np
-import pandas as pd
+from keras.callbacks import EarlyStopping
+
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
-from keras.callbacks import EarlyStopping
+from sklearn.metrics import brier_score_loss, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve,auc
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss
+
 
 # Reading tokenized notes from Panda Files
 df = pd.read_hdf('PandaFiles/tokenized_notes.h5')
@@ -37,8 +49,30 @@ X_train, X_test, y_train, y_test = train_test_split(notes, labels, test_size=0.3
 X_train = np.array(X_train)
 X_test = np.array(X_test)
 
-#define the LSTM baseline model for parameter tuning
-def LSTM_baseline(X_train, y_train, X_test, y_test, word_index, embedding_matrix, max_len, seed):
+# create a plot for the model
+def plot_model_history(model_history):
+    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    del fig
+    # summarize history for accuracy
+    axs[0].plot(range(1,len(model_history.history['acc'])+1),model_history.history['acc'])
+    axs[0].plot(range(1,len(model_history.history['val_acc'])+1),model_history.history['val_acc'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_history.history['acc'])+1),len(model_history.history['acc'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # summarize history for loss
+    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    plt.show()
+
+#LSTM Unidirectional model 
+def LSTM_Uni(X_train, y_train, X_test, y_test, word_index, embedding_matrix, max_len, seed):
     earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto')
     callbacks_list = [earlystop]
     model = Sequential()
@@ -51,10 +85,10 @@ def LSTM_baseline(X_train, y_train, X_test, y_test, word_index, embedding_matrix
     print(model.summary())
     kfold = list(StratifiedKFold(n_splits=5, shuffle=True, random_state=seed).split(X_train, y_train))
     model_infos = []
-    for i, (train, test) in enumerate(kfold):
-        print("Fit fold", i+1, " ***********************************")
-        model_info = model.fit(X_train[train], y_train[train], batch_size = 32, epochs = 2, verbose = 1, 
-                validation_data=(X_train[test], y_train[test]))
+    for i,(train, test) in enumerate(kfold):
+        print("Fit fold", i+1," ==========================================================================")
+        model_info=model.fit(X_train[train], y_train[train], epochs=2, batch_size=32, validation_data=(X_train[test], y_train[test]),
+                               callbacks=callbacks_list, verbose=1)
         print("Performance plot of fold {}:".format(i+1))
         plot_model_history(model_info)
         model_infos.append(model_info)
@@ -71,9 +105,12 @@ def LSTM_baseline(X_train, y_train, X_test, y_test, word_index, embedding_matrix
     metric.append(['accuracy',accuracy_score(y_test,y_pred_coded)])
 
     return y_pred, metric, model_infos
-        
 
-def GRU_model(embedding_matrix, word_index, max_len):
+     
+# GRU model
+def GRU_model(X_train, y_train, X_test, y_test, word_index, embedding_matrix, max_len, seed):
+    earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto')
+    callbacks_list = [earlystop]
     model = Sequential()
     model.add(Embedding(len(word_index)+1, 300, weights=[embedding_matrix], input_length=max_len, trainable=False))
     model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
@@ -81,9 +118,35 @@ def GRU_model(embedding_matrix, word_index, max_len):
     model.add(GRU(100))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+    print(model.summary())
+    kfold = list(StratifiedKFold(n_splits=5, shuffle=True, random_state=seed).split(X_train,y_train))
+    model_infos=[]
+    for i,(train, test) in enumerate(kfold):
+        print("Fit fold", i+1," ==========================================================================")
+        model_info=model.fit(X_train[train], y_train[train], epochs=100, batch_size=64, validation_data=(X_train[test], y_train[test]),
+                               callbacks=callbacks_list, verbose=1)
+        print("Performance plot of fold {}:".format(i+1))
+        plot_model_history(model_info)
+        model_infos.append(model_info)
+    
+    # Final evaluation of the model
+    y_pred=model.predict(X_test,verbose=1)
+    y_pred_coded=np.where(y_pred>0.5,1,0)
+    y_pred_coded=y_pred_coded.flatten()
+    
+    metric=[]
+    metric.append(['f1score',f1_score(y_test,y_pred_coded)])
+    metric.append(['precision',precision_score(y_test,y_pred_coded)])
+    metric.append(['recall',recall_score(y_test,y_pred_coded)])
+    metric.append(['accuracy',accuracy_score(y_test,y_pred_coded)])
 
+    return y_pred, metric, model_infos
+
+
+# LSTM_Bidirection model
 def LSTM_Bidir(embedding_matrix, word_index, max_len):
+    earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto')
+    callbacks_list = [earlystop]
     model = Sequential()
     model.add(Embedding(len(word_index)+1, 300, weights=[embedding_matrix], input_length=max_len, trainable=False))
     model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
@@ -91,9 +154,35 @@ def LSTM_Bidir(embedding_matrix, word_index, max_len):
     model.add(Bidirectional(LSTM(100), merge_mode='concat', weights=None))    
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+    print(model.summary())
+    kfold = list(StratifiedKFold(n_splits=5, shuffle=True, random_state=seed).split(X_train,y_train))
+    model_infos=[]
+    for i,(train, test) in enumerate(kfold):
+        print("Fit fold", i+1," **************************************************************************")
+        model_info=model.fit(X_train[train], y_train[train], epochs=100, batch_size=64, validation_data=(X_train[test], y_train[test]),
+                               callbacks=callbacks_list, verbose=1)
+        print("Performance plot of fold {}:".format(i+1))
+        plot_model_history(model_info)
+        model_infos.append(model_info)
+    
+    # Final evaluation of the model
+    y_pred=model.predict(X_test,verbose=1)
+    y_pred_coded=np.where(y_pred>0.5,1,0)
+    y_pred_coded=y_pred_coded.flatten()
+    
+    metric=[]
+    metric.append(['f1score',f1_score(y_test,y_pred_coded)])
+    metric.append(['precision',precision_score(y_test,y_pred_coded)])
+    metric.append(['recall',recall_score(y_test,y_pred_coded)])
+    metric.append(['accuracy',accuracy_score(y_test,y_pred_coded)])
 
+    return y_pred, metric, model_infos
+
+
+# CNN Model
 def CNN_model(embedding_matrix, word_index, max_len):
+    earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto')
+    callbacks_list = [earlystop]
     model = Sequential()
     model.add(Embedding(len(word_index)+1, 300, weights=[embedding_matrix], input_length=max_len, trainable=False))
     model.add(Conv1D(128, 5, activation='relu'))
@@ -101,7 +190,29 @@ def CNN_model(embedding_matrix, word_index, max_len):
     model.add(Dense(10, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    return model
+    print(model.summary())
+    kfold = list(StratifiedKFold(n_splits=5, shuffle=True, random_state=seed).split(X_train,y_train))
+    model_infos=[]
+    for i,(train, test) in enumerate(kfold):
+        print("Fit fold", i+1," **************************************************************************")
+        model_info=model.fit(X_train[train], y_train[train], epochs=100, batch_size=64, validation_data=(X_train[test], y_train[test]),
+                               callbacks=callbacks_list, verbose=1)
+        print("Performance plot of fold {}:".format(i+1))
+        plot_model_history(model_info)
+        model_infos.append(model_info)
+    
+    # Final evaluation of the model
+    y_pred=model.predict(X_test,verbose=1)
+    y_pred_coded=np.where(y_pred>0.5,1,0)
+    y_pred_coded=y_pred_coded.flatten()
+    
+    metric=[]
+    metric.append(['f1score',f1_score(y_test,y_pred_coded)])
+    metric.append(['precision',precision_score(y_test,y_pred_coded)])
+    metric.append(['recall',recall_score(y_test,y_pred_coded)])
+    metric.append(['accuracy',accuracy_score(y_test,y_pred_coded)])
+
+    return y_pred, metric, model_infos
 
 seed = 39
-y_pred, metric, model_infos = LSTM_baseline(X_train, y_train, X_test, y_test, word_index, embedding_matrix, max_len, seed)
+y_pred, metric, model_infos = LSTM_Uni(X_train, y_train, X_test, y_test, word_index, embedding_matrix, max_len, seed)
